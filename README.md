67d70a477312db5ac0f5f5862311570f9d5d69d4
unknown <davidlgjohansson@gmail.com>
2025-01-06 12:46:57 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # ARMA3_Swedish_houses
 Repository for managing the arma 3 mod of swedish houses pack
+Bästa videon om hur mnan gör ett ARMA mod jag hittat https://www.youtube.com/watch?v=GxGIgurkzW4
+
+Arma 3 toolbox till Blender https://github.com/AlwarrenSidh/ArmAToolbox
+
+texturdata som stödjs av ARMA3 https://community.bistudio.com/wiki/TexView_2_Manual
+Rekomenderade att använda är en diffusemap (_CO), normalmap (_NO), specular/roughness (_SM)
+    om man vill få det att bli snyggare är en Ambient shadow textur rekomenderad med (_AS)
+
+
+Hus skall stå på YX planet i blender. Detta blir markkontakt punkten in-game. För att få huset att se bättre ut rätt enkelt, lägg till en husgrund i -Z koordinat så det inte flyger på ojämn mark.
+
+
+Vad skall skapas i Blender?
+    ett hus skall existera i minst 3 LOD (fler är bättre). LOD är 'Level of detail' så när man går ifrån huset sjunker upplösningen på det så FPSen inte dör. 
+    En bra regel att följa är att varje LOD skall halvera mängden trianglar (trappas av snabbare om man är lat) den sista LODen skall bara vara en kub med rätt färg typ. Exempelvis ,
+    LOD 0 polygoner = 20'000. LOD 1 = 10'000 LOD 2 = 2'000 LOD 3 = 300
+
+    LODs skall heta 0,1,2,3 ... där 0 är den mest högupplösta. Se bild nedan
+
+    En kollisions geometri. Denna agerar som kollision för spelare. Bör hållas mycket konservativ i triangelvärden. 
+    Om så önskas kan en sepprat geometri för eldstrid göras, typ så man kan använda skytegluggar man inte kan gå ut igenom, annars används samma.
+    kollisions geometrin skall heta 'geom' och **måste** vara ett konvext skrov. Vill man ha ett icke-konvekt skrov får man dela upp det i Komponenter (se nedan).
+
+    Shadow genomeri. Geometi som används för att generera skuggor. skall heta 'shadow' Även denna skall vara konservativ med mängden trianglar.
+    Skuggeometrin bör ligga lite innanför LOD geometrin för att unvika buggar i belysningen, tänk 10% av väggtjockleken innanför eller en decimeter.
+
+    Texturering/UV mappning.
+    Det lättaste är att använda en texturkarta för hela huset, det skall gå att använda flera om man delar upp huset i Komponenter. Men är bökigare.
+    Följ vanlig Blender UV-mappningstekniker här, överlappa inte UV-mappen utanför bilden för att tilea dock.
+    Använd texturdata för Diffuse,Normal,Roughness i ett 2048x2048 format
+
+
+Komponenter:
+    Komponenter i Arma 3 behövs exempelvis för kollisionsgenomeri. Dessa är i stor sett bara en vertex-grupp i blender världen. Markera dina vertex som bildar ett convext skrov och lägg de till en vertexgrupp.
+    Dessa grupper MÅSTE heta Component01 , Component02 , Component03 osv.. det betyder att det max får finnas 99 Komponenter.
+    Det finns en inbyggd funktion i Arma 3 tools för detta som funkar ibland.
+    Bygg din geometri av simpla block och ta sedan och öppna "Arma3 tools" till höger om viewport, under "Arma tools" finns en "Create Geomety Components" som försöker göra Komponenter åt dig
+    Dubbelkolla så den gör rätt bara.
+
+
+Workflow i Blender:
+Installera ARMA3 toolbox till Blender. (se länk ovan)
+Starta sedan blender och töm hela scenen och modelera ett hus med <30'000 trianglar (inte en hård gräns. stora hus = mer trianglar)
+Se till att du inte har några N-gons innan du börjar med texturering.
+Sammla ihop texturer som du vill använda till huset exempelvis från https://ambientcg.com/ och kombinera de till en karta. (Substance painter vet jag funkar med)
+UV unwrap huset och sånt. kolla upp hur man gör det på youtube eller nått. (Jag kommer bara göra det sämre).
+Skapa LODar till huset, förslagsvis via 'Decimate' modifiern.
+Se till att LODarna ser ok ut, tänk på att de kommer synas på avstånd.
+Skapa kollisionsgenomeri och Skuggeometrin.
+
+Markera '0' och öppna Arma3 toolbaren till höger om din viewport och bocka i "Arma Object properties"
+LOD preset skall vara "Custom" på Resolution 0. Lägg sedan till en "Named Properties" vid namn "lodnoshadow" med värde 1.
+Gör samma steg för de andra LODarna med, fast ändra "Resolution" till upplösningen de skall ha (namnet)
+
+Markera 'geom' och sätt den som LOD preset "View Geometri"
+Markera 'shadow' och sätt den som LOD preset "Shadow Volume" med värde 0
+
+===mer specfika grejer som alla inte behöver göra.===
+
+Sätt Materialinställningarna på materialen via materials tabbel och välj filsökvägen för PAA filerna och RVmappen under "Arma toolbox material settings". endast synlig om en LOD är markerad.
+
---
+++ slask_house/config.cpp
@@ -0,0 +1,292 @@
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class slask_housetest
+	{
+		name = "Swedish_houses_pack";
+		author = "ertex";
+		requiredAddons[] = {"A3_Structures_F"};
+		requiredVersion = 0.1;
+		units[] = { "Land_slask_Generated2" 		,
+					"Land_slask_Generated3" 		,
+					"Land_slask_Generated4" 		,
+					"Land_slask_Generated5" 		,
+					"Land_slask_Generated6" 		,
+					"Land_slask_Generated6_gul" 	,
+					"Land_slask_Generated7" 		,
+					"Land_slask_Generated7_blue" 	, 
+					"Land_slask_Generated8" 		,
+					"Land_slask_Generated8_blue" 	,
+					"Land_slask_Generated8_gul" 	,
+					"Land_slask_Generated9" 		,
+					"Land_slask_Generated9_vit" 	,
+					"Land_slask_Generated10"		};
+		weapons[] = {};
+	};
+		
+};
+
+
+class CfgVehicles
+{
+	class House;
+
+	class Land_slask_Generated2: House
+		{
+			_generalMacro = "Land_slask_Generated2";
+			
+			displayName = "Swedish house 1"; // Name in editor
+			model = "slask_house\houses\generated_house_2.p3d";
+			editorPreview = "slask_house\previews\Land_slask_Generated2.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated3: House
+		{
+			_generalMacro = "Land_slask_Generated3";
+			displayName = "Swedish house 2"; // Name in editor
+			model = "slask_house\houses\generated_house_3.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated3.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated4: House
+		{
+			_generalMacro = "Land_slask_Generated4";
+			
+			displayName = "Swedish house 3"; // Name in editor
+			model = "slask_house\houses\generated_house_4.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated4.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated5: House
+		{
+			_generalMacro = "Land_slask_Generated5";
+			displayName = "Swedish house 4"; // Name in editor
+			model = "slask_house\houses\generated_house_5.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated5.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated6: House
+		{
+			_generalMacro = "Land_slask_Generated6";
+			displayName = "Swedish house 5"; // Name in editor
+			model = "slask_house\houses\generated_house_6.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated6.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+			
+		};
+		
+		
+	class Land_slask_Generated6_gul: House
+		{
+			_generalMacro = "Land_slask_Generated6_gul";
+			displayName = "Swedish house 5 yellow"; // Name in editor
+			model = "slask_house\houses\generated_house_6_gul.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated6_gul.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated7: House
+		{
+			_generalMacro = "Land_slask_Generated7";
+			displayName = "Swedish house 6"; // Name in editor
+			model = "slask_house\houses\generated_house_7.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated7.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+	class Land_slask_Generated7_blue: House
+		{
+			_generalMacro = "Land_slask_Generated7_blue";
+			
+			displayName = "Swedish house 6 blue"; // Name in editor
+			model = "slask_house\houses\generated_house_7_blue.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated7_blue.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated8: House
+		{
+			_generalMacro = "Land_slask_Generated8";
+			displayName = "Swedish house 7"; // Name in editor
+			model = "slask_house\houses\generated_house_8.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated8.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated8_blue: House
+		{
+			_generalMacro = "Land_slask_Generated8_blue";
+			displayName = "Swedish house 7 blue"; // Name in editor
+			model = "slask_house\houses\generated_house_8_blue.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated8_blue.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated8_gul: House
+		{
+			_generalMacro = "Land_slask_Generated8_gul";
+			displayName = "Swedish house 7 yellow"; // Name in editor
+			model = "slask_house\houses\generated_house_8_gul.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated8_gul.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+	class Land_slask_Generated9: House
+		{
+			_generalMacro = "Land_slask_Generated9";
+			displayName = "Swedish house 8 "; // Name in editor
+			model = "slask_house\houses\generated_house_9.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated9.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+	class Land_slask_Generated9_vit: House
+		{
+			_generalMacro = "Land_slask_Generated9_vit";
+			displayName = "Swedish house 8 white"; // Name in editor
+			model = "slask_house\houses\generated_house_9_vit.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			editorPreview = "slask_house\previews\Land_slask_Generated9_vit.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+		
+	class Land_slask_Generated10: House
+		{
+			_generalMacro = "Land_slask_Generated10";
+			displayName = "Swedish house 9"; // Name in editor
+			model = "slask_house\houses\generated_house_10.p3d";
+			icon = "slask_house\icons\Slaskmakten512.paa";
+			//editorPreview = "slask_house\previews\Land_slask_Generated10.jpg";
+			
+			mapSize = 20.27; // Scale of icon in editor
+			author = "ertex";
+			scope = 2;
+			
+			faction = "CIV_F";
+			editorCategory = "EdCat_Structures";
+			editorSubcategory = "EdSubcat_Residential_Village";
+			
+		};
+		
+		
+};
